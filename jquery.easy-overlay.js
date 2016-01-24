0d0cd9adfbe9249b8470e60b9e749763452ba664
nenomaz <nazzarenomazzitello@gmail.com>
2016-01-24 20:38:29 +0100

    Start of OOP incapsulation

--- jquery.easy-overlay.js
+++ jquery.easy-overlay.js
@@ -20,63 +20,71 @@ along with jQuery Easy Overlay.  If not, see <http://www.gnu.org/licenses/>.
 */
 (function( $ ){
 	$.fn.easyOverlay = function(action) {
+		
+		var overlayZIndex;
+		
+		function init(target) {
+			// Calculating OVERLAY DIV z-index
+			var targetZIndex = target.css('z-index');
+			if (targetZIndex == "auto")
+				overlayZIndex = "99999";
+			else 
+				overlayZIndex = parseFloat(targetZIndex) + 1;
+			
+			// Putting and Styling OVERLAY DIV if doesn't exist
+			if( !$("#jqueryEasyOverlayDiv").length ) {
+				var innerDiv = document.createElement('div');
+				$(innerDiv)
+					.css({ position: "absolute"})
+					.attr("id", "jqueryOverlayLoad")
+					.html("<i class='fa fa-spin fa-spinner fa-2x'></i>&nbsp;");
+				
+				var containerDiv = document.createElement('div');	
+				$(containerDiv)
+					.css({
+						display: "none",
+						position: "absolute",
+						background: "#fff"
+					})
+					.attr('id',"jqueryEasyOverlayDiv")
+					.append(innerDiv);
+				
+				$("body").append(containerDiv);
+			}
+			// Calculating Spinner Div positioning
+			var topOverlay = ((target.height()/2)-12);
+			var leftOverlay = ((target.width()/2)-12);
+			if(topOverlay < 0) topOverlay = 0;
+			$("#jqueryOverlayLoad").css({
+				top  : topOverlay,
+				left : leftOverlay
+			});
+			
+			target.data('jquery.easy-overlay', true);
+		}
+		
+		function start(target) {
+			// Restoring some CSS of OVERLAY DIV after every 'stop' because jquery.fadeOut method take off it
+			$("#jqueryEasyOverlayDiv").css({
+				opacity : 0.5,
+				zIndex  : overlayZIndex,
+				top     :  target.offset().top,
+				left    : target.offset().left,
+				width   : target.outerWidth(),
+				height  : target.outerHeight()
+			});
+			
+			// Show OVERLAY DIV
+			$("#jqueryEasyOverlayDiv").fadeIn();
+		}
+		
 		switch (action) {
 			case 'start':
 				{
-					var $this = $(this);
-		
-					if($this.length <= 0) return;
-					
-					// Calculating OVERLAY DIV z-index
-					var overlayZIndex;
-					var targetZIndex = $this.css('z-index');
-					if (targetZIndex == "auto")
-						overlayZIndex = "99999";
-					else 
-						overlayZIndex = parseFloat(targetZIndex) + 1;
-					
-					// Putting and Styling OVERLAY DIV if doesn't exist
-					if( !$("#jqueryEasyOverlayDiv").length ) {
-						var innerDiv = document.createElement('div');
-						$(innerDiv)
-							.css({ position: "absolute"})
-							.attr("id", "jqueryOverlayLoad")
-							.html("<i class='fa fa-spin fa-spinner fa-2x'></i>&nbsp;");
-						
-						var containerDiv = document.createElement('div');	
-						$(containerDiv)
-							.css({
-								display: "none",
-								position: "absolute",
-								background: "#fff"
-							})
-							.attr('id',"jqueryEasyOverlayDiv")
-							.append(innerDiv);
-						
-						$("body").append(containerDiv);
+					if(this.length > 0) {
+						init(this);
+						start(this);
 					}
-					
-					// Restoring some CSS of OVERLAY DIV after every 'overlayout' because jquery.fadeOut method take off it
-					$("#jqueryEasyOverlayDiv").css({
-						opacity : 0.5,
-						zIndex  : overlayZIndex,
-						top     :  $this.offset().top,
-						left    : $this.offset().left,
-						width   : $this.outerWidth(),
-						height  : $this.outerHeight()
-					});
-					
-					// Calculating Spinner Div positioning
-					var topOverlay = (($this.height()/2)-12);
-					var leftOverlay = (($this.width()/2)-12);
-					if(topOverlay < 0) topOverlay = 0;
-					$("#jqueryOverlayLoad").css({
-						top  : topOverlay,
-						left : leftOverlay
-					});
-					
-					// Show OVERLAY DIV
-					$("#jqueryEasyOverlayDiv").fadeIn();
 				}
 				break;
 			case 'stop':
